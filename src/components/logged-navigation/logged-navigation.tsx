3768fb5ab30605857e78bef20b5e494bec159c6a
JuliannaLazareva <juliannalazareva123@gmail.com>
2024-02-29 16:50:35 +0300

    Добавляет LoggedNavigation

---
+++ src/components/logged-navigation/logged-navigation.tsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../constants/const';
+
+function LoggedNavigation() {
+  return (
+    <ul className="header__nav-list">
+      <li className="header__nav-item user">
+        <Link
+          className="header__nav-link header__nav-link--profile"
+          to={AppRoute.Favorites}
+        >
+          <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+          <span className="header__user-name user__name">
+            Oliver.conner@gmail.com
+          </span>
+          <span className="header__favorite-count">3</span>
+        </Link>
+      </li>
+      <li className="header__nav-item">
+        <Link className="header__nav-link" to={AppRoute.Root}>
+          <span className="header__signout">Sign out</span>
+        </Link>
+      </li>
+    </ul>
+  );
+}
+
+export default LoggedNavigation;
