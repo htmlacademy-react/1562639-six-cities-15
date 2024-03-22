d2f795110030084c28e34b84f283ab8003ba03f5
JuliannaLazareva <juliannalazareva123@gmail.com>
2024-03-22 17:03:48 +0300

    Подключает Redux Toolkit

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -3,37 +3,25 @@ import type { FullOffer } from '../types/offer';
 
 import { CITIES } from '../constants/const';
 import { offers } from '../mock/offers';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
 type OffersState = {
   city: CityName;
   offers: FullOffer[];
-}
+};
 
 const initialState: OffersState = {
   city: CITIES[0].name,
   offers,
 };
 
-const enum ActionType {
-  SetCity = 'offers/setCity',
-}
+const setCity = createAction<CityName>('offers/setCity');
 
-const setCity = (city: CityName) => ({
-  payload: city,
-  type: ActionType.SetCity,
-});
+const reducer = createReducer(initialState,
+  (builder) => {
+    builder.addCase(setCity, (state, action) => {
+      state.city = action.payload;
+    });
+  });
 
-function reducer(state: OffersState = initialState, action: {payload: unknown; type: ActionType}):
-OffersState {
-  switch (action.type) {
-    case ActionType.SetCity:
-      return {
-        ...state,
-        city: action.payload as CityName,
-      };
-    default:
-      return state;
-  }
-}
-
-export {reducer, setCity};
+export { reducer, setCity };
