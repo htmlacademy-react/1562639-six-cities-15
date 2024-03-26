76c64adf6558bd82cfed5346a9f6c0a1a74d09d1
JuliannaLazareva <juliannalazareva123@gmail.com>
2024-03-26 10:08:18 +0300

    Добавляет состояние для страницы без офферов

--- src/components/main-empty-page/main-empty-page.tsx
+++ src/components/main-empty-page/main-empty-page.tsx
@@ -1,39 +1,23 @@
-import Header from '../header/header';
-import LocationList from '../location-list/location-list';
-import { AuthorizationStatus } from '../../constants/const';
+import { CityName } from '../../constants/const';
 
 type MainEmptyProps = {
-  authorizationStatus: AuthorizationStatus;
-}
+  city: CityName;
+};
 
-function MainEmptyPage({authorizationStatus} : MainEmptyProps): JSX.Element {
+function MainEmptyPage({ city }: MainEmptyProps): JSX.Element {
   return (
-    <div className="page page--gray page--main">
-      <Header authorizationStatus={authorizationStatus} />
-      <main className="page__main page__main--index page__main--index-empty">
-        <h1 className="visually-hidden">Cities</h1>
-        <div className="tabs">
-          <section className="locations container">
-            <LocationList />
-          </section>
+    <>
+      <section className="cities__no-places">
+        <div className="cities__status-wrapper tabs__content">
+          <b className="cities__status">No places to stay available</b>
+          <p className="cities__status-description">
+            We could not find any property available at the moment in
+            {city}
+          </p>
         </div>
-        <div className="cities">
-          <div className="cities__places-container cities__places-container--empty container">
-            <section className="cities__no-places">
-              <div className="cities__status-wrapper tabs__content">
-                <b className="cities__status">No places to stay available</b>
-                <p className="cities__status-description">
-                  We could not find any property available at the moment in
-                  Dusseldorf
-                </p>
-              </div>
-            </section>
-            <div className="cities__right-section" />
-          </div>
-        </div>
-      </main>
-    </div>
-
+      </section>
+      <div className="cities__right-section" />
+    </>
   );
 }
 
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -12,6 +12,7 @@ import { useActionCreators, useAppSelector } from '../../hooks/store';
 import { offersActions, offersSelectors } from '../../store/slices/offers';
 import classNames from 'classnames';
 import { MouseEvent } from 'react';
+import MainEmptyPage from '../../components/main-empty-page/main-empty-page';
 
 type MainPageProps = {
   city: CityName;
@@ -19,7 +20,7 @@ type MainPageProps = {
 };
 
 function MainPage({ city, authorizationStatus }: MainPageProps): JSX.Element {
-  const {setActiveId} = useActionCreators(offersActions);
+  const { setActiveId } = useActionCreators(offersActions);
 
   const handleMouseEnter = (evt: MouseEvent<HTMLElement>) => {
     const target = evt.currentTarget as HTMLElement;
@@ -45,7 +46,11 @@ function MainPage({ city, authorizationStatus }: MainPageProps): JSX.Element {
         <title>6 Cities</title>
       </Helmet>
       <Header authorizationStatus={authorizationStatus} />
-      <main className={classNames('page__main', 'page__main--index', {'page__main--index-empty' : isEmpty})}>
+      <main
+        className={classNames('page__main', 'page__main--index', {
+          'page__main--index-empty': isEmpty,
+        })}
+      >
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
@@ -53,57 +58,72 @@ function MainPage({ city, authorizationStatus }: MainPageProps): JSX.Element {
           </section>
         </div>
         <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">
-                {currentOffers.length} places to stay in {city}
-              </b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width={7} height={4}>
-                    <use xlinkHref="#icon-arrow-select" />
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex={0}
-                  >
-                    Popular
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Top rated first
-                  </li>
-                </ul>
-              </form>
-              <div className="cities__places-list places__list tabs__content">
-                {currentOffers.map((offer) => (
-                  <PlaceCard
+          <div
+            className={classNames('container', 'cities__places-container', {
+              'cities__places-container--empty': isEmpty,
+            })}
+          >
+            {isEmpty ? (
+              <MainEmptyPage city={city} />
+            ) : (
+              <>
+                <section className="cities__places places">
+                  <h2 className="visually-hidden">Places</h2>
+                  <b className="places__found">
+                    {currentOffers.length} place
+                    {currentOffers.length > 1 && 's'} to stay in {city}
+                  </b>
+                  <form className="places__sorting" action="#" method="get">
+                    <span className="places__sorting-caption">Sort by</span>
+                    <span className="places__sorting-type" tabIndex={0}>
+                      Popular
+                      <svg
+                        className="places__sorting-arrow"
+                        width={7}
+                        height={4}
+                      >
+                        <use xlinkHref="#icon-arrow-select" />
+                      </svg>
+                    </span>
+                    <ul className="places__options places__options--custom places__options--opened">
+                      <li
+                        className="places__option places__option--active"
+                        tabIndex={0}
+                      >
+                        Popular
+                      </li>
+                      <li className="places__option" tabIndex={0}>
+                        Price: low to high
+                      </li>
+                      <li className="places__option" tabIndex={0}>
+                        Price: high to low
+                      </li>
+                      <li className="places__option" tabIndex={0}>
+                        Top rated first
+                      </li>
+                    </ul>
+                  </form>
+                  <div className="cities__places-list places__list tabs__content">
+                    {currentOffers.map((offer) => (
+                      <PlaceCard
+                        environment={ComponentEnvironment.Cities}
+                        key={`${offer.id}`}
+                        {...offer}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                      />
+                    ))}
+                  </div>
+                </section>
+                <div className="cities__right-section">
+                  <Map
                     environment={ComponentEnvironment.Cities}
-                    key={`${offer.id}`}
-                    {...offer}
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}
+                    offers={offers}
+                    city={city}
                   />
-                ))}
-              </div>
-            </section>
-            <div className="cities__right-section">
-              <Map
-                environment={ComponentEnvironment.Cities}
-                offers={offers}
-                city={city}
-              />
-            </div>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </main>
