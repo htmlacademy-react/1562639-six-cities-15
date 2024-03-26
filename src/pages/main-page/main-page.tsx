91fad64ec9441891e23e9bfc047998f9a4239830
JuliannaLazareva <juliannalazareva123@gmail.com>
2024-03-26 11:37:04 +0300

    Реализует сортировку офферов

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -11,8 +11,10 @@ import PlaceCard from '../../components/place-card/place-card';
 import { useActionCreators, useAppSelector } from '../../hooks/store';
 import { offersActions, offersSelectors } from '../../store/slices/offers';
 import classNames from 'classnames';
-import { MouseEvent } from 'react';
+import { MouseEvent, useState } from 'react';
 import MainEmptyPage from '../../components/main-empty-page/main-empty-page';
+import Sort from '../../components/sort/sort';
+import { SortOption } from '../../components/sort/const';
 
 type MainPageProps = {
   city: CityName;
@@ -21,6 +23,7 @@ type MainPageProps = {
 
 function MainPage({ city, authorizationStatus }: MainPageProps): JSX.Element {
   const { setActiveId } = useActionCreators(offersActions);
+  const [activeSort, setActiveSort] = useState(SortOption.Popular);
 
   const handleMouseEnter = (evt: MouseEvent<HTMLElement>) => {
     const target = evt.currentTarget as HTMLElement;
@@ -39,6 +42,20 @@ function MainPage({ city, authorizationStatus }: MainPageProps): JSX.Element {
   const currentOffers = offersByCity[city] ?? [];
 
   const isEmpty = currentOffers.length === 0;
+
+  let sortedOffers = currentOffers;
+
+  if (activeSort === SortOption.PriceLowToHigh) {
+    sortedOffers = currentOffers.toSorted((a, b) => a.price - b.price);
+  }
+
+  if (activeSort === SortOption.PriceHighToLow) {
+    sortedOffers = currentOffers.toSorted((a, b) => b.price - a.price);
+  }
+
+  if (activeSort === SortOption.TopRatedFirst) {
+    sortedOffers = currentOffers.toSorted((a, b) => b.rating - a.rating);
+  }
 
   return (
     <div className="page page--gray page--main">
@@ -73,38 +90,9 @@ function MainPage({ city, authorizationStatus }: MainPageProps): JSX.Element {
                     {currentOffers.length} place
                     {currentOffers.length > 1 && 's'} to stay in {city}
                   </b>
-                  <form className="places__sorting" action="#" method="get">
-                    <span className="places__sorting-caption">Sort by</span>
-                    <span className="places__sorting-type" tabIndex={0}>
-                      Popular
-                      <svg
-                        className="places__sorting-arrow"
-                        width={7}
-                        height={4}
-                      >
-                        <use xlinkHref="#icon-arrow-select" />
-                      </svg>
-                    </span>
-                    <ul className="places__options places__options--custom places__options--opened">
-                      <li
-                        className="places__option places__option--active"
-                        tabIndex={0}
-                      >
-                        Popular
-                      </li>
-                      <li className="places__option" tabIndex={0}>
-                        Price: low to high
-                      </li>
-                      <li className="places__option" tabIndex={0}>
-                        Price: high to low
-                      </li>
-                      <li className="places__option" tabIndex={0}>
-                        Top rated first
-                      </li>
-                    </ul>
-                  </form>
+                  <Sort current={activeSort} setter={setActiveSort} />
                   <div className="cities__places-list places__list tabs__content">
-                    {currentOffers.map((offer) => (
+                    {sortedOffers.map((offer) => (
                       <PlaceCard
                         environment={ComponentEnvironment.Cities}
                         key={`${offer.id}`}
