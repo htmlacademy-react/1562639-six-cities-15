3f3a295af391ee60fce85cb5f4100ee3046c9794
JuliannaLazareva <juliannalazareva123@gmail.com>
2024-03-27 09:46:18 +0300

    Рефакторит favorites-page

--- src/components/logged-navigation/logged-navigation.tsx
+++ src/components/logged-navigation/logged-navigation.tsx
@@ -1,7 +1,12 @@
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../constants/const';
+import { useAppSelector } from '../../hooks/store';
+import { offersSelectors } from '../../store/slices/offers';
 
 function LoggedNavigation() {
+
+  const offers = useAppSelector(offersSelectors.offers);
+  const favoritesOffers = offers.filter((offer) => offer.isFavorite);
   return (
     <ul className="header__nav-list">
       <li className="header__nav-item user">
@@ -13,7 +18,7 @@ function LoggedNavigation() {
           <span className="header__user-name user__name">
             Oliver.conner@gmail.com
           </span>
-          <span className="header__favorite-count">3</span>
+          <span className="header__favorite-count">{favoritesOffers.length}</span>
         </Link>
       </li>
       <li className="header__nav-item">
---
+++ src/pages/favorites-page/favorites-list.tsx
@@ -0,0 +1,38 @@
+import { Link } from 'react-router-dom';
+import { CITIES, ComponentEnvironment } from '../../constants/const';
+import { Offer } from '../../types/offer';
+import PlaceCard from '../../components/place-card/place-card';
+
+type FavoritesListProps = {
+  offers: Offer[];
+};
+
+function FavoritesList({ offers }: FavoritesListProps): JSX.Element {
+  return (
+    <ul className="favorites__list">
+      {CITIES.map((city) => {
+        const offersByCity = offers.filter((offer) => offer.city.name === city.name);
+        if (offersByCity.length) {
+          return (
+            <li className="favorites__locations-items" key={city.slug}>
+              <div className="favorites__locations locations locations--current">
+                <div className="locations__item">
+                  <Link className="locations__item-link" to={`/${city.slug}`}>
+                    <span>{city.name}</span>
+                  </Link>
+                </div>
+              </div>
+              <div className="favorites__places">
+                {offersByCity.map((offer) => (
+                  <PlaceCard environment={ComponentEnvironment.Favorites} {...offer} key={offer.id} />
+                ))}
+              </div>
+            </li>
+          );
+        }
+      })}
+    </ul>
+  );
+}
+
+export default FavoritesList;
