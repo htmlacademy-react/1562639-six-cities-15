25dadaa89b8f177c4851bb467c51d40d5bf4ece6
JuliannaLazareva <juliannalazareva123@gmail.com>
2024-04-03 17:57:46 +0300

    Добавляет компонент react-toastify

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -5,24 +5,11 @@ import { AppRoute, CITIES } from '../../constants/const';
 import MainPage from '../../pages/main-page/main-page';
 import FavoritesPage from '../../pages/favorites-page/favorite-page';
 import LoginPage from '../../pages/login-page/login-page';
-// import OfferPage from '../../pages/offer-page/offer-page';
+import OfferPage from '../../pages/offer-page/offer-page';
 import NotFoundPage from '../../pages/not-found-page/not-found-page';
 import { HelmetProvider } from 'react-helmet-async';
-import { useActionCreators } from '../../hooks/store';
-import { userActions } from '../../store/slices/user';
-import { getToken } from '../../services/token';
-import { useEffect } from 'react';
 
 function App(): JSX.Element {
-  const {checkAuth} = useActionCreators(userActions);
-
-  const token = getToken();
-  useEffect(() => {
-    if (token) {
-      checkAuth();
-    }
-  }, [token, checkAuth]);
-
   return (
     <HelmetProvider>
       <BrowserRouter>
@@ -58,10 +45,10 @@ function App(): JSX.Element {
               </ProtectedRoute>
             }
           />
-          {/* <Route
+          <Route
             path={AppRoute.Offer}
             element={<OfferPage />}
-          /> */}
+          />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
