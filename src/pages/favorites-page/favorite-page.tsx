e8d60144379549574bf036384992f7da061ddcb7
JuliannaLazareva <juliannalazareva123@gmail.com>
2024-04-07 20:12:43 +0300

    Реализует добавление/удаление в избранное

--- src/pages/favorites-page/favorite-page.tsx
+++ src/pages/favorites-page/favorite-page.tsx
@@ -2,39 +2,37 @@ import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
 import { Helmet } from 'react-helmet-async';
 import { useAppSelector } from '../../hooks/store';
-import { offersSelectors } from '../../store/slices/offers';
 import classNames from 'classnames';
 import FavoritesEmptyPage from '../../components/favorites-empty-page/favorites-empty-page';
 import FavoritesList from './favorites-list';
+import { favoritesSelectors } from '../../store/slices/favorites';
 
 
 function FavoritesPage(): JSX.Element {
-  const offers = useAppSelector(offersSelectors.offers);
-  const favoritesOffers = offers.filter((offer) => offer.isFavorite);
-
-  const isEmpty = favoritesOffers.length === 0;
+  const offers = useAppSelector(favoritesSelectors.favorites);
+  const hasFavorites = offers.length > 0;
 
   return (
-    <div className={classNames('page', { 'page--favorites-empty': isEmpty })}>
+    <div className={classNames('page', { 'page--favorites-empty': hasFavorites })}>
       <Helmet>
         <title>6 Cities - favorites</title>
       </Helmet>
       <Header />
       <main
         className={classNames('page__main', 'page__main--favorites', {
-          'page__main--favorites-empty': isEmpty,
+          'page__main--favorites-empty': hasFavorites,
         })}
       >
         <div className="page__favorites-container container">
           <section className={classNames('favorites', {
-            'favorites--empty' : isEmpty
+            'favorites--empty' : hasFavorites
           })}
           >
-            <h1 className={classNames('favorites__title', {'visually-hidden' : isEmpty})}>{isEmpty ? 'Favorites (empty)' : 'Saved listing'}</h1>
-            {isEmpty ? (
-              <FavoritesEmptyPage />
+            <h1 className={classNames('favorites__title', {'visually-hidden' : !hasFavorites})}>{hasFavorites ? 'Saved listing' : 'Favorites (empty)'}</h1>
+            {hasFavorites ? (
+              <FavoritesList offers={offers} />
             ) : (
-              <FavoritesList offers={favoritesOffers} />
+              <FavoritesEmptyPage />
             )}
           </section>
         </div>
